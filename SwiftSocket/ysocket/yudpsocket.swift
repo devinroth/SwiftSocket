dc9be62625b9f895ffc184a4e4ace36463d824a6
Devin Roth <devinroth@outlook.com>
2016-07-08 15:00:49 -0700

    Update to Swift3 Xcode8Beta2

--- SwiftSocket/ysocket/yudpsocket.swift
+++ SwiftSocket/ysocket/yudpsocket.swift
@@ -30,21 +30,21 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Foundation
 
-@_silgen_name("yudpsocket_server") func c_yudpsocket_server(host:UnsafePointer<Int8>,port:Int32) -> Int32
-@_silgen_name("yudpsocket_recive") func c_yudpsocket_recive(fd:Int32,buff:UnsafePointer<UInt8>,len:Int32,ip:UnsafePointer<Int8>,port:UnsafePointer<Int32>) -> Int32
-@_silgen_name("yudpsocket_close") func c_yudpsocket_close(fd:Int32) -> Int32
+@_silgen_name("yudpsocket_server") func c_yudpsocket_server(_ host:UnsafePointer<Int8>,port:Int32) -> Int32
+@_silgen_name("yudpsocket_recive") func c_yudpsocket_recive(_ fd:Int32,buff:UnsafePointer<UInt8>,len:Int32,ip:UnsafePointer<Int8>,port:UnsafePointer<Int32>) -> Int32
+@_silgen_name("yudpsocket_close") func c_yudpsocket_close(_ fd:Int32) -> Int32
 @_silgen_name("yudpsocket_client") func c_yudpsocket_client() -> Int32
-@_silgen_name("yudpsocket_get_server_ip") func c_yudpsocket_get_server_ip(host:UnsafePointer<Int8>,ip:UnsafePointer<Int8>) -> Int32
-@_silgen_name("yudpsocket_sentto") func c_yudpsocket_sentto(fd:Int32,buff:UnsafePointer<UInt8>,len:Int32,ip:UnsafePointer<Int8>,port:Int32) -> Int32
-@_silgen_name("enable_broadcast") func c_enable_broadcast(fd:Int32)
+@_silgen_name("yudpsocket_get_server_ip") func c_yudpsocket_get_server_ip(_ host:UnsafePointer<Int8>,ip:UnsafePointer<Int8>) -> Int32
+@_silgen_name("yudpsocket_sentto") func c_yudpsocket_sentto(_ fd:Int32,buff:UnsafePointer<UInt8>,len:Int32,ip:UnsafePointer<Int8>,port:Int32) -> Int32
+@_silgen_name("enable_broadcast") func c_enable_broadcast(_ fd:Int32)
 
 public class UDPClient: YSocket {
     public override init(addr a:String,port p:Int){
         super.init()
-        let remoteipbuff:[Int8] = [Int8](count:16,repeatedValue:0x0)
+        let remoteipbuff:[Int8] = [Int8](repeating: 0x0,count: 16)
         let ret=c_yudpsocket_get_server_ip(a, ip: remoteipbuff)
         if ret==0{
-            if let ip=String(CString: remoteipbuff, encoding: NSUTF8StringEncoding){
+            if let ip=String(cString: remoteipbuff, encoding: String.Encoding.utf8){
                 self.addr=ip
                 self.port=p
                 let fd:Int32=c_yudpsocket_client()
@@ -99,12 +99,12 @@ public class UDPClient: YSocket {
     *
     * send nsdata
     */
-    public func send(data d:NSData)->(Bool,String){
+    public func send(data d:Data)->(Bool,String){
         if let fd:Int32=self.fd{
-            var buff:[UInt8] = [UInt8](count:d.length,repeatedValue:0x0)
-            d.getBytes(&buff, length: d.length)
-            let sendsize:Int32=c_yudpsocket_sentto(fd, buff: buff, len: Int32(d.length), ip: self.addr,port: Int32(self.port))
-            if sendsize==Int32(d.length){
+            var buff:[UInt8] = [UInt8](repeating: 0x0,count: d.count)
+            (d as NSData).getBytes(&buff, length: d.count)
+            let sendsize:Int32=c_yudpsocket_sentto(fd, buff: buff, len: Int32(d.count), ip: self.addr,port: Int32(self.port))
+            if sendsize==Int32(d.count){
                 return (true,"send success")
             }else{
                 return (false,"send error")
@@ -134,15 +134,14 @@ public class UDPServer:YSocket{
         }
     }
     //TODO add multycast and boardcast
-    public func recv(expectlen:Int)->([UInt8]?,String,Int){
+    public func recv(_ expectlen:Int)->([UInt8]?,String,Int){
         if let fd:Int32 = self.fd{
-            var buff:[UInt8] = [UInt8](count:expectlen,repeatedValue:0x0)
-            var remoteipbuff:[Int8] = [Int8](count:16,repeatedValue:0x0)
+            var buff:[UInt8] = [UInt8](repeating: 0x0,count: expectlen)
+            var remoteipbuff:[Int8] = [Int8](repeating: 0x0,count: 16)
             var remoteport:Int32=0
             let readLen:Int32=c_yudpsocket_recive(fd, buff: buff, len: Int32(expectlen), ip: &remoteipbuff, port: &remoteport)
             let port:Int=Int(remoteport)
-            var addr:String=""
-            if let ip=String(CString: remoteipbuff, encoding: NSUTF8StringEncoding){
+            if let ip=String(cString: remoteipbuff, encoding: String.Encoding.utf8){
                 addr=ip
             }
             if readLen<=0{
